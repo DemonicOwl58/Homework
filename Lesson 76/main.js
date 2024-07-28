bcea963e288714cadc50db91ebc68d588a704da2
DemonicOwl58 <156656911+DemonicOwl58@users.noreply.github.com>
2024-07-28 17:06:42 +0300

    Update main.js

--- Lesson 76/main.js
+++ Lesson 76/main.js
@@ -1,7 +1,7 @@
 const firstName = 'Eduard'
 const lastName = 'Trachuk'
-let age = '24'
-let height = '176'
+let age = 24
+let height = 176
 let city = 'Penza'
 
 console.log ( firstName, lastName, age, 'y.o.', height, 'cm', city)
