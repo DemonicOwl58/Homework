9632b68e3e95cb61a0e294032aa82470d32d219c
DemonicOwl58 <156656911+DemonicOwl58@users.noreply.github.com>
2024-07-31 19:54:05 +0300

    add lesson 80

---
+++ Lesson 80/main.js
@@ -0,0 +1,61 @@
+// 1 задача
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+
+let dividingLine_1 = "///////////////////////////////////////////"
+console.log(dividingLine_1)
+
+
+
+
+
+
+
+
+// 2 задача
+for (let x = 10; x >= 1; x = x - 1) {
+    console.log(x)
+}
+
+let dividingLine_2 = "///////////////////////////////////////////"
+console.log(dividingLine_2)
+
+
+
+
+
+
+
+
+
+
+
+// 3.1 задача
+let y = 0
+
+while (y < 12) {
+    y = y + 2
+    console.log(y)
+}
+
+let dividingLine_3 = "///////////////////////////////////////////"
+console.log(dividingLine_3)
+
+
+
+
+
+
+
+
+
+
+
+// 3.2 задача
+let z = 11
+
+while (z > 1) {
+    z = z - 1
+    console.log(z)
+}
